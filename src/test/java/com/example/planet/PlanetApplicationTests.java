2d52d0ce7e2b83f1168ebc7a7c1fbf86d1832c5f
John Doe <alexander.burcovschi@gmail.com>
2024-07-10 21:00:18 +0300

    spring.io

---
+++ src/test/java/com/example/planet/PlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
